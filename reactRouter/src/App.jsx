c5eebdf926faff2d193e47bb6b4928ef22fd2790
Amit Kumar <amitk200415@gmail.com>
2024-10-20 17:50:54 +0530

    reactRouter

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ reactRouter/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Contact from "./Contact/Contact";
+import About from "./About/About";
+import Home from "./Home/Home";
+import User from "./User/User";
+
+export {Header, Footer, Contact, About, Home, User}
